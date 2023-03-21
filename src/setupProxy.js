a20c5c1efa035331f8ea24da38c05e51173bc0d3
Son Tung <son.tung.tn12321@gmail.com>
2023-03-21 10:45:54 +0700

    proxy-2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/api',
+        'https://game-rpg.herokuapp.com/api/v1/auth/login',
         createProxyMiddleware({
             target: 'https://game-rpg.herokuapp.com',
             changeOrigin: true,
