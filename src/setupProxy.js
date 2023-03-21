3bdb67b6d8688cc72991cd5a4b25113efc5bbf72
Son Tung <son.tung.tn12321@gmail.com>
2023-03-21 11:26:24 +0700

    proxy4

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware('https://game-rpg.herokuapp.com/api/v1/auth/login', {
-            target: 'https://dead-or-alive-chi.vercel.app',
+        'https://game-rpg.herokuapp.com/api/v1/auth/login',
+        createProxyMiddleware({
+            target: 'https://project-game-rpg.herokuapp.com/swagger-ui/index.html?fbclid=IwAR0voiPpsI2Cxj1yScqpIPEsLJ49d66hV9V0BiRpKzbtIf3KpR76xZ_uFEM#/',
             changeOrigin: true,
         }),
     );
