da1ab3be3409a956fc8f5ce5642aeb88df3f735a
Son Tung <son.tung.tn12321@gmail.com>
2023-03-25 16:22:20 +0700

    taigame

--- src/pages/Home/Login/Download/UserDow.jsx
+++ src/pages/Home/Login/Download/UserDow.jsx
@@ -2,17 +2,40 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './UserDow.scss';
 
+const ZIP_FILE_URL = 'https://dead-or-alive-chi.vercel.app/logo192.png';
 const cx = classNames.bind(styles);
 
 function UserDow({ children }) {
+    const downloadFileAtURL = (url) => {
+        fetch(url)
+            .then((response) => response.blob())
+            .then((blob) => {
+                const blonURL = window.URL.createObjectURL(new Blob([blob]));
+                const filename = url.split('/').pop();
+                const aTag = document.createElement('a');
+                aTag.href = blonURL;
+                aTag.setAttribute('download', filename);
+                document.body.appendChild(aTag);
+                aTag.click();
+                aTag.remove();
+            });
+    };
     return (
         <div>
             <div className={cx('dow-content')}>
                 <h1 className="heading">Tải Game</h1>
                 <div className="headerdow">TẢI BẢN FULL</div>
-                <a href="https://drive.google.com/drive/u/1/trash" className="btnDow">
+                <button
+                    className="btnDow"
+                    onClick={() => {
+                        downloadFileAtURL(ZIP_FILE_URL);
+                    }}
+                >
                     Download
-                </a>
+                </button>
+                {/* <a href="https://drive.google.com/drive/u/1/trash" className="btnDow">
+                    Download
+                </a> */}
                 <div className="headerdow">Bản cài đặt DOA "Dead or Alive"</div>
                 <div className="txt">Kiến nghị:</div>
                 <div className="txtdetail">- Tắt Antivirus trước khi chạy file.</div>
